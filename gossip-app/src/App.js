44e7bc55d970129d5af10ef694c55767fb9ee0df
ryan macbook <rvtsang@gmail.com>
2018-11-06 23:26:37 -0800

    Add firebase package and config object

--- gossip-app/src/App.js
+++ gossip-app/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyA8JK0NeMnrbHoFD0GEndZ1_K7njoWg2kg",
+    authDomain: "gossip-8188d.firebaseapp.com",
+    databaseURL: "https://gossip-8188d.firebaseio.com",
+    projectId: "gossip-8188d",
+    storageBucket: "gossip-8188d.appspot.com",
+    messagingSenderId: "23354869566"
+};
 
 class App extends Component {
   render() {
