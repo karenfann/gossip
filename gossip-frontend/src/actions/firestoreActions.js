33ed6d6a1ed99ae8509f169f64b34ea27c3619cc
ryan macbook <rvtsang@gmail.com>
2018-11-30 10:57:44 -0800

    Add fetch gossip function

--- gossip-frontend/src/actions/firestoreActions.js
+++ gossip-frontend/src/actions/firestoreActions.js
@@ -6,6 +6,36 @@ db.settings({
   timestampsInSnapshots: true
 });
 
+export const getGossip = (userLocation, radius) => {
+    // Get all the documents, for each compute the distance between userLocation and 
+    // the location specified by the document
+    return new Promise((resolve) => {
+        // TODO return a promise and resolve with the docsInRange!
+        db.collection('gossips').get()
+        .then(querySnapshot => {
+            let docsInRange = []
+            console.log('getGossip query snapshot: ', querySnapshot)
+            querySnapshot.forEach(doc => {    
+                // Compute distance between
+                let dx = doc.data().location.longitude - userLocation.longitude
+                let dy = doc.data().location.latitude - userLocation.latitude
+                let distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+                console.log("Distance between: ", distance)
+
+                if (distance <= radius) {
+                    docsInRange.push(doc)
+                }
+            })
+
+            console.log('Documents in range', docsInRange)
+            resolve(docsInRange)
+        })
+        .catch(e => {
+            console.log("Error getting gossips: ", e)
+        })
+    })
+}
+
 export const postGossip = (text, location) => {
     return db.collection('gossips').add({
         text,
@@ -25,4 +55,6 @@ const fetchAllDocuments = () => {
           });
       });
   }
+
+
 
--- gossip-frontend/src/components/Input/Input.js
+++ gossip-frontend/src/components/Input/Input.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
+import firebase from 'firebase'
+import { getGossip } from '../../actions/firestoreActions'
 
 import './Input.scss'
 
 class Input extends React.Component {
     render() {
+
         return (
             <div className="input-wrapper">
-                <button className="form-button" type="button">submit</button>
+                <button className="form-button" 
+                        type="button" 
+                        onClick={ () => {
+                            getGossip(new firebase.firestore.GeoPoint(34.0650788, -118.4502537), 100)
+                            .then(responsesInRange => {
+                                console.log("Got allll the gossip: ", responsesInRange)
+                            })
+                        }}>submit</button>
                 <div className="form-input-wrapper">
                     <input className="form-input" placeholder="spill the tea" />
                 </div>
             </div>
+
+            
         );
     }
 }
