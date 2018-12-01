a2778df7a114b745abca6f52c7d49bfe6871b871
katiecai <katiecai@g.ucla.edu>
2018-12-01 13:53:37 -0800

    new comment

--- gossip-frontend/src/components/CommentInput/CommentInput.js
+++ gossip-frontend/src/components/CommentInput/CommentInput.js
@@ -28,6 +28,7 @@ class CommentInput extends React.Component {
             this.setState({
                 value: ""
             })
+            this.props.handleNewComment()
         }
     }
 
--- gossip-frontend/src/components/CommentSection/CommentSection.js
+++ gossip-frontend/src/components/CommentSection/CommentSection.js
@@ -4,8 +4,22 @@ import './CommentSection.scss'
 import CommentInput from '../CommentInput'
 
 class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayedComments: this.props.comments
+        }
+        this.handleNewComment = this.handleNewComment.bind(this);
+    }
+
+    handleNewComment() {
+        this.setState({
+            displayedComments: this.props.comments
+        })
+    }
+
     render() {
-        let comments = this.props.comments.map(text => {
+        let comments = this.state.displayedComments.map(text => {
             return (
                 <div className="post-comment">
                     {text}
@@ -15,7 +29,7 @@ class CommentSection extends React.Component {
         return (
             <div className="post-comment-section">
                 <div className="post-comments">{comments}</div>
-                <CommentInput docId={this.props.docId}/>
+                <CommentInput docId={this.props.docId} handleNewComment={this.handleNewComment}/>
             </div>
         )
     }
