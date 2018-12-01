f8f1b68b82ab52e6804442f8afff67772ccadbb5
Ram Goli <ramsgoli@gmail.com>
2018-12-01 15:14:58 -0800

    change where babel polyfill is loaded

--- gossip-frontend/webpack.config.js
+++ gossip-frontend/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = env => ({
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: './src/index.js',
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
