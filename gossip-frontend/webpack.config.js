cc9943ea32f4b6c45ff43ecfb7a581182b3aa7f8
Ram Goli <ramsgoli@gmail.com>
2018-12-02 18:54:02 -0800

    clean up

--- gossip-frontend/webpack.config.js
+++ gossip-frontend/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = env => ({
-  entry: './src/index.js',
+    entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
