78916d101ebdf35c3fa1ef7ac9fde3d828500b06
Ram Goli <ramsgoli@gmail.com>
2018-12-02 21:45:58 -0800

    clean up getGossips function

--- gossip-frontend/src/actions/firestoreActions.js
+++ gossip-frontend/src/actions/firestoreActions.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+
 const db = firebase.firestore()
 
 import computeRadius from '../helpers/MathHelpers'
@@ -17,41 +18,25 @@ db.settings({
  * @param {Geolocation} userLocation
  * @param {int} radius in miles
  */
-export const getGossips = (userLocation, radius, timeLimit=24) => {
-    return new Promise((resolve) => {
+export const getGossips = (userLocation, radius=2, timeLimit=24) => {
         // Get all the documents, for each compute the distance between userLocation and
         // the location specified by the document
 
         // TODO: convert radius in miles to lat/long
         // TODO: verify the redux action works correctly
 
-        db.collection('gossips').get()
-        .then(querySnapshot => {
-            let docsInRange = []
-            querySnapshot.forEach(doc => {
-                const distance = computeRadius(userLocation.latitude, userLocation.longitude, doc.data().location.latitude, doc.data().location.longitude)
-                console.log("Distance between: ", distance)
+    const secondsLimit = 3600 * timeLimit
+    const now = Math.round((new Date).getTime() / 1000) 
+    const query = db.collection('gossips').where('timestamp', '<=', now - secondsLimit)
 
-                // Compute time since post
-                const secondsPerHour = 60 * 60
-                const postTimeLimit = 24 * secondsPerHour
-                const postTimestamp = doc.data().timestamp.seconds
-                const now = Math.round((new Date).getTime() / 1000) 
-                const timeSincePost = now - postTimestamp
-                console.log("Time since post: ", timeSincePost)
-
-                if (distance <= radius && timeSincePost <= postTimeLimit) 
-                {
-                    docsInRange.push(doc)
-                }
-            })
-
-            console.log('Documents in range', docsInRange)
-            resolve(docsInRange)
-        })
-        .catch(e => {
-            console.log("Error getting gossips: ", e)
-        })
+    return db.collection('gossips').get().then(querySnapshot => {
+        return querySnapshot.docs.reduce((acc, doc) => {
+            const distance = computeRadius(userLocation.latitude, userLocation.longitude, doc.data().location.latitude, doc.data().location.longitude)
+            if (distance <= radius) {
+                acc.push(doc)
+            }
+            return acc
+        }, [])
     })
 }
 
